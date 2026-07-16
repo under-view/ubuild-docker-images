349c0c7eef81c566c3b8b559e0f422e0e2cc3463
Vincent Davis Jr <vince@underview.tech>
2026-07-15 23:43:20 -0400

    containers[yp-ubuntu24]: include setup yocto environment script
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ containers/yocto-project/ubuntu-24.04/setup-oe-enironment.sh
@@ -0,0 +1,19 @@
+export DISTRO="$1"
+
+if [[ -z "${DISTRO}" ]]; then
+	echo "[x] DISTRO not set"
+	exit 1
+fi
+
+export OE_TERMINAL="screen -A"
+
+export DL_DIR="${HOME}/downloads"
+export SOURCES_DIR="${HOME}/sources"
+export BUILD_DIR="${HOME}/build-output"
+export SSTATE_DIR="${HOME}/sstate-cache"
+
+core_count=$(nproc)
+export PARALLEL_MAKE="-j $((core_count / 2))"
+export BB_NUMBER_THREADS=$((core_count / 2))
+
+export BB_ENV_PASSTHROUGH_ADDITIONS="DL_DIR SSTATE_DIR"
