47993a9f7b6f92070db207a7db233524e53255ca
Vincent Davis Jr <vince@underview.tech>
2026-07-18 10:34:14 -0400

    set-bash-promt.sh: update PS1 to print distro
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- containers/yocto-project/ubuntu-24.04/set-bash-prompt.sh
+++ containers/yocto-project/ubuntu-24.04/set-bash-prompt.sh
@@ -1,6 +1,5 @@
 set_bash_prompt() {
-	fs_mode=$(mount | sed -n -e "s/^\/dev\/.* on \/ .*(\(r[w|o]\).*/\1/p")
-	PS1='\[\e[91;1m\]\u@\h ${fs_mode:+($fs_mode)}\[\033[00m\]: \[\e[33;1m\]\W \[\e[32;1m\]\$ \[\033[0m\]'
+	PS1='\[\e[91;1m\]\u@oe-distro(${DISTRO:-nodistro})\[\033[00m\]: \[\e[33;1m\]\W \[\e[32;1m\]\$ \[\033[0m\]'
 }
 
 PROMPT_COMMAND=set_bash_prompt
