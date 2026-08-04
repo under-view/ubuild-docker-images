077f120752e59d9c421ab86c21a477cdaa06aa85
Vincent Davis Jr <vince@underview.tech>
2026-08-03 22:06:38 -0400

    yp-ubuntu: use DISTRO var when creating build output
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- containers/yocto-project/ubuntu-24.04/setup-oe-env.sh
+++ containers/yocto-project/ubuntu-24.04/setup-oe-env.sh
@@ -16,8 +16,8 @@ export OE_TERMINAL="screen -A"
 
 export DL_DIR="${HOME}/downloads"
 export SOURCES_DIR="${HOME}/sources"
-export BUILD_DIR="${HOME}/build-output"
 export SSTATE_DIR="${HOME}/sstate-cache"
+export BUILD_DIR="${HOME}/build-output/${DISTRO}"
 
 core_count=$(nproc)
 export PARALLEL_MAKE="-j $((core_count / 2))"
@@ -27,4 +27,5 @@ export BB_ENV_PASSTHROUGH_ADDITIONS="DL_DIR SSTATE_DIR SOURCES_DIR"
 
 export TEMPLATECONF="$(find "${SOURCES_DIR}" -type f -name "local.conf.sample" -printf "%h\n" | grep -w "${DISTRO}")"
 
+mkdir -p "${BUILD_DIR}"
 source "$(find "${SOURCES_DIR}" -type f -name "oe-init-build-env")" "${BUILD_DIR}"
